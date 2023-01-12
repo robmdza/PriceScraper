ccc243a0ffb0f08363fb8716e2d7c3fe8698cb39
Rober <licrober@gmail.com>
2023-01-11 23:47:36 -0600

    new structure, functions and main file

--- MLSearch.py
+++ MLSearch.py
@@ -23,51 +23,65 @@ def save_to_database(title, price, shipping, condition, url):
     conn.close()
 
 
-url = "https://listado.mercadolibre.com.mx/computacion/" \
-      "%22quest-2%22_" \
-      "PriceRange_4000-7000_NoIndex_True#applied_filter_id%3Dcategory%26" \
-      "applied_filter_name%3DCategor%C3%ADas%26applied_filter_order%3D4%26" \
-      "applied_value_id%3DMLM1648%26applied_value_name%3DComputaci%C3%B3n%26" \
-      "applied_value_order%3D2%26applied_value_results%3D4%26is_custom%3Dfalse"
-response = requests.get(url)
-if response.status_code == 200:
-    with open('page.html', 'w') as f:
-        f.write(response.text)
-else:
-    print('Request Failed')
+def execute_search():
+    url = "https://listado.mercadolibre.com.mx/computacion/" \
+          "%22quest-2%22_" \
+          "PriceRange_4000-7000_NoIndex_True#applied_filter_id%3Dcategory%26" \
+          "applied_filter_name%3DCategor%C3%ADas%26applied_filter_order%3D4%26" \
+          "applied_value_id%3DMLM1648%26applied_value_name%3DComputaci%C3%B3n%26" \
+          "applied_value_order%3D2%26applied_value_results%3D4%26is_custom%3Dfalse"
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open('page.html', 'w') as f:
+            f.write(response.text)
+            return response.text
+    else:
+        print('Request Failed')
+        return "Failed"
 # print(response.ok)
-soup = BeautifulSoup(response.text, "html.parser")
-num_resultsElement = soup.find('span',
-                               class_='ui-search-search-result__quantity-results shops-custom-secondary-font')
-num_results = num_resultsElement \
-    .get_text()
-num = num_results.split(' ')
 
-# Get All the items from the results page
-all_items = soup.find_all('h2', class_='ui-search-item__title shops__item-title')
-for item in all_items:
-    print(item.get_text())
 
-all_elements = soup.find_all('div',
-                             class_='andes-card andes-card--flat andes-card--default ui-search-result '
-                                    'shops__cardStyles ui-search-result--core andes-card--padding-default')
-for element in all_elements:
-    title = element.find('h2', class_="ui-search-item__title shops__item-title").text
-    price = element.find('span', class_="price-tag-text-sr-only").text.replace(' pesos', '')
-    url = element.find('a', class_="ui-search-item__group__element shops__items-group-details ui-search-link")['href']
-    try:
-        condition = element.find('span',
-                                 class_='ui-search-item__group__element ui-search-item__details '
-                                        'shops__items-group-details').text
-    except:
-        condition = 'none'
-    shipping = element.find('p',
-                            class_="ui-search-item__shipping ui-search-item__shipping--free shops__item-shipping-free").text
-    print(title, price, shipping, condition, url)
-    save_to_database(title, price, shipping, condition, url)
-    # print(element.get_text())
+def parse_data(response):
+    soup = BeautifulSoup(response, "html.parser")
+    num_results_element = soup.find('span',
+                                    class_='ui-search-search-result__quantity-results shops-custom-secondary-font')
+    num_results = num_results_element \
+        .get_text()
+    num = num_results.split(' ')
 
-print(num[0])
+    # Get All the items from the results page
+    all_items = soup.find_all('h2', class_='ui-search-item__title shops__item-title')
+    for item in all_items:
+        print(item.get_text())
+
+    all_elements = soup.find_all('div',
+                                 class_='andes-card andes-card--flat andes-card--default ui-search-result '
+                                        'shops__cardStyles ui-search-result--core andes-card--padding-default')
+    for element in all_elements:
+        title = element.find('h2', class_="ui-search-item__title shops__item-title").text
+        price = element.find('span', class_="price-tag-text-sr-only").text.replace(' pesos', '')
+        url = element.find('a', class_="ui-search-item__group__element shops__items-group-details ui-search-link")['href']
+        try:
+            condition = element.find('span',
+                                     class_='ui-search-item__group__element ui-search-item__details '
+                                            'shops__items-group-details').text
+        except:
+            condition = 'none'
+        try:
+            shipping = element.find('p',
+                                    class_="ui-search-item__shipping ui-search-item__shipping--free "
+                                           "shops__item-shipping-free").text
+        except:
+            shipping = 'none'
+        print(title, price, shipping, condition, url)
+        save_to_database(title, price, shipping, condition, url)
+        # print(element.get_text())
+
+
+def start():
+    page = execute_search()
+    parse_data(page)
+    # print(num[0])
 
 
 
--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from MLSearch import start
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}'+ "Dance")  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}  Dance')  # Press ⌘F8 to toggle the breakpoint
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi()
-    MLSearch()
+    # print_hi()
+    start()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
